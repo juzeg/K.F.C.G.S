6fb5a2d2e8c5a1ff012debe74f0b801518926f97
juzeg <kaptur.witold@gmail.com>
2016-11-04 12:25:37 +0100

    implemented idea of functionality

--- misssion_managment.h
+++ misssion_managment.h
@@ -1,10 +1,26 @@
 #ifndef MISSSION_MANAGMENT_H_INCLUDED
 #define MISSSION_MANAGMENT_H_INCLUDED
+#include <fstream>
 int Mission_protocol ()
 {
-     std::cout << "Data succesfully saved" <<std::endl;
-
-     return 0;
+     bool data_saving_succes;
+     ofstream log_file( "flight_log.txt" );
+     
+     
+     
+     
+     
+     
+     
+     
+     if (data_saving_succes == TRUE) 
+     {
+     std::cout << "Data succesfully saved" <<std::endl:
+     return 1;
+    }
+     else 
+     {std::cout << "Data was unable to be saved correctly" << std::endl ;
+     return 0;}
 }
 
 
